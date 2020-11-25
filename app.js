4ab2cee70b924454280ed2839b532651d6ab0df7
catherineblu <e.ropleva@gmail.com>
2020-11-25 08:43:17 +0200

    animating the card

---
+++ app.js
@@ -0,0 +1,10 @@
+//movement animation
+const card = document.querySelector('.card');
+const container = document.querySelector('.container');
+
+//moving animation event
+container.addEventListener('mousemove', (e) => {
+    let xAxis = (window.innerWidth / 2 - e.pageX) /25;
+    let yAxis = (window.innerHeight / 2 - e.pageY)/25;
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
